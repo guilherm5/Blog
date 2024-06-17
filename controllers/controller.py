9f7f1303bbd5bd34338f82c0255ebccc669a9451
guilherm5 <estudosrodriguesti@gmail.com>
2024-06-16 21:35:13 -0300

    feat: Adicionando validação de email no backend, caso email ja exista na base de dados o usuario nao podera realizar cadastro

--- controllers/controller.py
+++ controllers/controller.py
@@ -9,11 +9,20 @@ def create_user(user: Usuario):
     try:
         conn = connect_database()  
         cursor = conn.cursor()
+
+        # verifica se ja existe usuario cadastrado
+        cursor.execute("SELECT u.email_usuario FROM usuario u WHERE email_usuario = %s", (user.email_usuario,))
+        email_existing = cursor.fetchone()
+        
+        if email_existing:
+            raise HTTPException(status_code=400, detail="Email ja cadastrado.")     
+
         cursor.execute("INSERT INTO usuario (nome_usuario, email_usuario, senha_usuario) VALUES (%s, %s, %s)", (user.nome_usuario, user.email_usuario, user.encript_nome(user.senha_usuario)))
-        conn.commit()  # Aplica a transação no banco de dados
-        return {"message": "Usuário criado com sucesso"}
+
+        conn.commit()  
+        return {"mensagem": "sucesso"}
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Erro ao cadastrar usuário: {str(e)}")
+        return e
     finally:
         cursor.close()
         conn.close()  
