5b107ef2cfaebc79d3874347f1644bc30e7942e9
guilherm5 <estudosrodriguesti@gmail.com>
2024-06-23 17:25:30 -0300

    feat: adicionando injeção de dependencias na rota de update(adicionei so na rota de update mas funcionaria pra todas as outras), terminando funcao de login e retornando parametros via state Reqyest do fastapi

--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Depends, HTTPException
 from models.model_usuario import Usuario
 from controllers.crud_usuario import create_user, get_users, delete_user, update_user
-from controllers.security import login_user
+from controllers.security import login_user, get_current_user
 app = APIRouter()   
 
 # Rotas - CRUD
@@ -18,8 +18,10 @@ def user_delete(user: Usuario):
     return delete_user(user)
 
 @app.put("/update-user")
-def user_update(user: Usuario):
-    return update_user(user)
+def put_user(user: Usuario, request: Request, current_user: dict = Depends(get_current_user)):
+    if not current_user:
+        raise HTTPException(status_code=401, detail="Não autorizado")
+    return update_user(user, request)
 
 # Rota - LOGIN 
 @app.post("/login-user")
